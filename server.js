4411d771442692e6a2e89ba6ac999c05597fa11a
Your Name <you@example.com>
2018-01-26 18:54:32 +0300

    Some commit message

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-﻿var fs = require('fs');
+﻿var fs = require('fs');var http = require('http');
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var url = require('url');
